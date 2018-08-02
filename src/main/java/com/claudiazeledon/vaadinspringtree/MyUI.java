fb3b76f6bab7d8bbb5fcc685099a23f69fbbdf32
Claudia Zeledon <cjzeledon@gmail.com>
2018-08-02 11:24:47 -0400

    First Commit

---
+++ src/main/java/com/claudiazeledon/vaadinspringtree/MyUI.java
@@ -0,0 +1,8 @@
+package com.claudiazeledon.vaadinspringtree;
+
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI
+public class MyUI extends UI{
+}
---
+++ src/main/java/com/claudiazeledon/vaadinspringtree/VaadinspringtreeApplication.java
@@ -0,0 +1,12 @@
+package com.claudiazeledon.vaadinspringtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinspringtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinspringtreeApplication.class, args);
+	}
+}
